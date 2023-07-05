51d5b50913854a1496fcea244d58890f160014a5
DrChhun <Chhunfindsome1@gmail.com>
2023-07-05 14:45:08 +0700

    finished add new task to array

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,10 @@ import { DateDisplay } from "./components/Date/DateDisplay";
 import { InputTask } from "./components/Task/InputTask";
 import { TaskDetail } from "./components/Task/TaskDetail";
 import { Tab } from "./components/ToggleTab/Tab";
-import { decrement, increment } from "./redux/features/todoRedux";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 const Page = () => {
   const todo = useSelector((state: any) => state.todos.value);
-  const dispath = useDispatch();
   return (
     <div className="container">
       <div className="flex justify-between py-12">
@@ -17,15 +15,19 @@ const Page = () => {
         <InputTask />
       </div>
       <Tab className="py-8" title="ALL" notification={12} />
-      <TaskDetail
-        title={"Complete exercise on page 32"}
-        date={"23 may 2023"}
-        category={"Basic Task"}
-      />
-
-      <h1>Here is redux {todo}</h1>
-      <button onClick={() => dispath(decrement())}>minus</button>
-      <button onClick={() => dispath(increment())}>Add more</button>
+      {todo.map((x: any, index: number) => (
+        <>
+          {x != "" && (
+            <div key={index} className="py-4">
+              <TaskDetail
+                title={x}
+                date={"23 may 2023"}
+                category={"Basic Task"}
+              />
+            </div>
+          )}
+        </>
+      ))}
     </div>
   );
 };

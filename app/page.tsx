aa6d5847fe326f8eea560067326b26401f89f20e
DrChhun <Chhunfindsome1@gmail.com>
2023-07-05 14:09:07 +0700

    added rtk to application

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import { DateDisplay } from "./components/Date/DateDisplay";
 import { InputTask } from "./components/Task/InputTask";
 import { TaskDetail } from "./components/Task/TaskDetail";
 import { Tab } from "./components/ToggleTab/Tab";
+import { decrement, increment } from "./redux/features/todoRedux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Page = () => {
+  const todo = useSelector((state: any) => state.todos.value);
+  const dispath = useDispatch();
   return (
     <div className="container">
       <div className="flex justify-between py-12">
@@ -16,6 +22,10 @@ const Page = () => {
         date={"23 may 2023"}
         category={"Basic Task"}
       />
+
+      <h1>Here is redux {todo}</h1>
+      <button onClick={() => dispath(decrement())}>minus</button>
+      <button onClick={() => dispath(increment())}>Add more</button>
     </div>
   );
 };

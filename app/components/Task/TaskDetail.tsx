65fbba44585ca615be5193d84f7327379c88b162
DrChhun <93082266+DrChhun@users.noreply.github.com>
2023-07-05 15:36:14 +0700

    Update TaskDetail.tsx

--- app/components/Task/TaskDetail.tsx
+++ app/components/Task/TaskDetail.tsx
@@ -1,8 +1,15 @@
 import { TaskProps } from "@/app/Type/Todo";
 
-export const TaskDetail: React.FC<TaskProps> = ({ title, date, category }) => {
+export const TaskDetail: React.FC<TaskProps> = ({
+  title,
+  date,
+  category,
+  className,
+}) => {
   return (
-    <div className="bg-white shadow-md py-8 px-16 rounded-md max-w-3xl mx-auto">
+    <div
+      className={`bg-white shadow-md py-8 px-16 rounded-md max-w-3xl mx-auto ${className}`}
+    >
       <div className="flex items-center justify-between">
         <h2 className="text-black text-2xl">{title}</h2>
         <div className="bg-green-300 py-2 px-4 text-green-600 text-xl rounded-full">
